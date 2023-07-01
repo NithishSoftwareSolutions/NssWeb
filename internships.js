37af4adeb10c82dfc8fbdf42e1413941c6b6dd2c
Jayaaditya <jayaadityathota@gmail.com>
2023-07-01 12:02:55 +0530

    “Newchangesinnsssliderandrevies

---
+++ internships.js
@@ -0,0 +1,41 @@
+let i = 0;
+let images = [];
+//let time = 3000;
+
+images [0] = 'images/Intern1.png';
+images [1] = 'images/Intern2.jpg';
+images [2] = 'images/Intern3.png';
+images [3] = 'images/Intern4.webp';
+
+
+prev = () =>{
+    document.slide.src = images[i];
+    if(i < images.length -1){
+        i-=-1;
+    }else{
+        i=0;
+    }
+}
+
+next = () =>{
+    document.slide.src = images[i];
+    if(i < images.length -1){
+        i+=1;
+    }else{
+        i=0;
+    }
+}
+//---------Auto slider--------
+//  changeImg = ()=>{
+//     // document.slide.src = images[i];
+//     // if(i<images.length -1){
+//     //     i++;
+//     // }else{
+//     //     i=0;
+//     // }
+
+//     setTimeout("changeImg()",4000);
+// }
+
+
+window.onload = prev;
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// --------------------- Google sheets connecting to data of the NSS ------------->
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzDnv7rfVp1PC_cMtJ4Ql1OC71EwHb7BBqbr1ewg0WHVH4OubOUj3xldz_90DphR_zi/exec'
 const form = document.forms['submit-to-google-sheet']
@@ -15,3 +16,37 @@ form.addEventListener('submit', e => {
     })
     .catch(error => console.error('Error!', error.message))
 })
+
+// ---------------------- Top Reviews ------------------->
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
+
+// ------------------ Ratings ---------------------->
+
+// let icons = document.querySelectorAll('.icon a');
+// icons.forEach((item, index1) =>{
+//   item.addEventListener('click',
+//    ()=>{
+//     icons.forEach((icon, index2) => {
+//         index1 >= index2 ? icon.classList.add('active') : icon.classList.remove('active')
+//     })
+//   })
+// })
+
+// ------------------- Reviws -------------------
